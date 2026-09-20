9dc580a59da1c19bccf373424132dbb0f346579a
sirdzat iman <sirdzatiman80@gmail.com>
2026-09-20 12:36:10 +0700

    Menambahkan class Setter dan Getter pada class motor

--- src/tk2_klmpk5/TK2_Klmpk5.java
+++ src/tk2_klmpk5/TK2_Klmpk5.java
@@ -14,7 +14,19 @@ public class TK2_Klmpk5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //// Membuat objek Motor dengan data awal
+        Motor motor1 = new Motor("Yamaha", "Merah", "Vega R", 2);
+        //Menggunakan Setter untuk mengubah nilai atribut
+        motor1.setMerk("Honda");
+        motor1.setWarna("Putih");
+        motor1.setTipe("Vario");
+        motor1.setJumlahRoda(2);
+        //Menggunakan Getter untuk mengambil nilai atribut
+        System.out.println("Merk : " + motor1.getMerk());
+        System.out.println("Warna : " + motor1.getWarna());
+        System.out.println("Tipe : " + motor1.getTipe());
+        System.out.println("Jumlah Roda : " + motor1.getJumlahRoda());
+
     }
     
 }
