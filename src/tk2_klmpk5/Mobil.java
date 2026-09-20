c109c4127f9e546253c0ca51ba290df079c74f71
dira <nadiravisa9@gmail.com>
2026-09-20 10:32:17 +0700

    Menambahkan class Mobil Branch Nadira

---
+++ src/tk2_klmpk5/Mobil.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tk2_klmpk5;
+
+/**
+ *
+ * @author NADIRA
+ */
+public class Mobil {
+    String merk;
+    String warna;
+    String tipe;
+    String jumlahRoda;
+
+    public Mobil(String merk, String warna, String tipe, String jumlahRoda) {
+        this.merk = merk;
+        this.warna = warna;
+        this.tipe = tipe;
+        this.jumlahRoda = jumlahRoda;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public String getJumlahRoda() {
+        return jumlahRoda;
+    }
+
+    public void setJumlahRoda(String jumlahRoda) {
+        this.jumlahRoda = jumlahRoda;
+    }
+    
+}
